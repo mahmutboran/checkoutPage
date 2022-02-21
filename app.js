9db798e72c26c275ba579400ac3f318833cf6ef3
BORAN08 <mahmutboran08@gmail.com>
2022-02-21 17:49:14 +0300

    addToCart added

---
+++ app.js
@@ -0,0 +1,72 @@
+let plus = document.querySelectorAll(".fa-plus")
+let minus = document.querySelectorAll(".fa-minus")
+let product = document.querySelectorAll("#product-quantity")
+
+/* productPainel.addEventListener("click",(e)=>{
+console.log( e.target.classList.contains("fa-minus"))
+}) */
+
+
+plus.forEach(e => {
+    const button = e.parentElement
+    button.addEventListener("click", () => {
+        let quantity = button.parentElement.children[1].innerHTML++
+        let productPrice = button.parentElement.parentElement.children[1].children[0].children[0].innerText
+        let _productTotal = (quantity + 1) * productPrice
+        button.parentElement.parentElement.children[4].innerHTML = _productTotal.toFixed(2)
+
+        buyDetail()
+
+
+
+    })
+});
+
+minus.forEach(e => {
+    let button = e.parentElement
+    button.addEventListener("click", () => {
+        if (+(button.parentElement.children[1].innerHTML) > 1) {
+            let quantity = button.parentElement.children[1].innerHTML--
+            let productPrice = button.parentElement.parentElement.children[1].children[0].children[0].innerText
+            let _productTotal = (quantity - 1) * productPrice
+            button.parentElement.parentElement.children[4].innerHTML = _productTotal.toFixed(2)
+            buyDetail()
+        }
+    })
+});
+
+//function
+const buyDetail = () => {
+    const productTotal = document.querySelectorAll(".product-line-price")
+    const _subtotal = document.getElementById("cart-subtotal").children[1]
+    const _tax = document.getElementById("cart-tax").children[1]
+    const _total = document.getElementById("cart-total").children[1]
+    const _shipping = document.getElementById("cart-shipping").children[1]
+
+    total = 0;
+    productTotal.forEach(e => {
+        total += Number(e.innerHTML)
+    })
+
+    _subtotal.innerHTML = total.toFixed(2)
+    _tax.innerHTML = (total * 18 / 100).toFixed(2)
+    _shipping.innerHTML = "15.00"
+    _total.innerHTML = (Number(_subtotal.innerHTML) + Number(_tax.innerHTML) + Number(_shipping.innerHTML)).toFixed(2)
+}
+buyDetail()
+
+let _remove = document.querySelectorAll(".remove-product")
+_remove.forEach(e => {
+    e.addEventListener("click", () => {
+        e.parentElement.parentElement.parentElement.remove()
+        buyDetail()
+    })
+})
+
+
+
+
+
+
+
+
--- checkout.js
+++ checkout.js
@@ -1,68 +1,103 @@
-let plus = document.querySelectorAll(".fa-plus")
-let minus = document.querySelectorAll(".fa-minus")
-let product = document.querySelectorAll("#product-quantity")
+let productPainel = document.getElementById("product-painel");
+/* *****************product******************* */
 
+productPainel.addEventListener("click", (e) => {
 
-plus.forEach(e => {
-    let button = e.parentElement
-    button.addEventListener("click", () => {
-        let quantity = button.parentElement.children[1].innerHTML++
-        let productPrice = button.parentElement.parentElement.children[1].children[0].children[0].innerText
-        let _productTotal = (quantity + 1) * productPrice
-        button.parentElement.parentElement.children[4].innerHTML = _productTotal.toFixed(2)
+    let quantity = e.target.parentElement.parentElement.children[1];
+    let productPrice = e.target.parentElement.parentElement.parentElement.children[1].children[0].children[0];
+    let productTotal = e.target.parentElement.parentElement.parentElement.children[4];
 
-        buyDetail()
-    })
-});
+    if (e.target.classList.contains("fa-plus")) {
+        quantity.innerText++
+        productTotal.innerText = (quantity.innerText * productPrice.innerText).toFixed(2)
 
-minus.forEach(e => {
-    let button = e.parentElement
-    button.addEventListener("click", () => {
-        if (+(button.parentElement.children[1].innerHTML) > 1) {
-            let quantity = button.parentElement.children[1].innerHTML--
-            let productPrice = button.parentElement.parentElement.children[1].children[0].children[0].innerText
-            let _productTotal = (quantity - 1) * productPrice
-            button.parentElement.parentElement.children[4].innerHTML = _productTotal.toFixed(2)
-            buyDetail()
-        }
-    })
-});
+    } if (e.target.classList.contains("fa-minus") && quantity.innerText > 1) {
+        quantity.innerText--
+        productTotal.innerText = (quantity.innerText * productPrice.innerText).toFixed(2)
 
-//function
-let buyDetail = () => {
-    let productTotal = document.querySelectorAll(".product-line-price")
+    } if (e.target.classList.contains("remove-product")) {
+        e.target.parentElement.parentElement.parentElement.remove()
+    }
+
+    buyDetail()
+
+})
+
+/* ******************buyDatail**************** */
+
+const buyDetail = () => {
+    const _productTotal = document.querySelectorAll(".product-line-price")
     const _subtotal = document.getElementById("cart-subtotal").children[1]
     const _tax = document.getElementById("cart-tax").children[1]
     const _total = document.getElementById("cart-total").children[1]
-    let _shipping = document.getElementById("cart-shipping").children[1]
-
+    const _shipping = document.getElementById("cart-shipping").children[1]
     total = 0;
-    productTotal.forEach(e => {
-        total += Number(e.innerHTML)
-    })
-
+    _productTotal.forEach(e => { total += (+e.innerHTML) })
     _subtotal.innerHTML = total.toFixed(2)
-    _tax.innerHTML = (total * 18 / 100).toFixed(2)
+    _tax.innerHTML = (total * 0.18).toFixed(2)
     _shipping.innerHTML = "15.00"
-    _total.innerHTML = (Number(_subtotal.innerHTML) + Number(_tax.innerHTML) + Number(_shipping.innerHTML)).toFixed(2)
+    _total.innerHTML = ((+_subtotal.innerHTML) + (+_tax.innerHTML) + (+_shipping.innerHTML)).toFixed(2)
 }
 buyDetail()
 
+/* *********************add to cart************** */
+const addToCart = document.getElementById("customer-form").children[1];
+let nameProduct = document.getElementById("name");
+const priceProduct = document.getElementById("price");
+const quantityProduct = document.getElementById("quantity");
+let products = document.getElementById("products")
 
-let remove = () => {
-    let _remove = document.querySelectorAll(".remove-product")
-    _remove.forEach(e => {
-        e.addEventListener("click", () => {
-            e.parentElement.parentElement.parentElement.remove()
-            buyDetail()
-        })
-    })
 
+
+
+const createProduct = (name, price, _quantity) => {
+    return `  
+    <img src="" alt="${name}">
+    <div class="product-info">
+        <h2>${name}</h2>
+        <div class="product-price">
+            <p><strong>${price}</strong> <span class="line-through">${price * 130 / 100}</span></p>
+        </div>
+        <div class="quantity-controller">
+            <button>
+                <i class="fas fa-minus"></i>
+            </button>
+            <p id="product-quantity">${_quantity}</p> 
+            <button>
+                <i class="fas fa-plus"></i>
+            </button>
+        </div>
+        <div class="product-removal">
+            <button class="remove-product">
+                Remove
+            </button>
+        </div>
+        <div class="product-line-price">${price * _quantity}</div>
+
+    </div>
+`
 }
-remove()
+
+
+addToCart.addEventListener("click", () => {
+
+    if (nameProduct.value && priceProduct.value && quantityProduct.value) {
+        let productdiv = document.createElement("div");
+        productdiv.className = "product";
+        productdiv.innerHTML = createProduct(nameProduct.value, priceProduct.value, quantityProduct.value)
+        products.prepend(productdiv)
+        form.reset()
+    } else {
+        alert("Must be filled out")
+    }
+    buyDetail()
+})
 
 
 
-
-
+/* ***********form prevent ************* */
+const form = document.querySelector("form")
+form.addEventListener("click", (e) => {
+    e.preventDefault()
+})
 
